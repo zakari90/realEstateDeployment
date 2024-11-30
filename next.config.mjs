f837bad3c46e275c6e917fd5819d6e6322c70cd3
zakari90 <zakariazinedine1@gmail.com>
2024-11-30 15:19:35 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,27 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'via.placeholder.com'
+            },            
+            {
+              protocol: 'https',
+              hostname: "utfs.io"
+            }, {
+              protocol: 'https',
+              hostname: "img.clerk.com"
+            },
+            
+            {
+              protocol:"https",
+              hostname:"images.unsplash.com"
+            }
+          ],
+    }
+};
  
 export default withNextIntl(nextConfig);
